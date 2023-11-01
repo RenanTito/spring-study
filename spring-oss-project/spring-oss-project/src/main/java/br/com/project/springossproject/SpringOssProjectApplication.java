3d96f97bd57045cb724453a3d7f07681ed4e1012
Renan Henrique Garcia Tito <renan.tito2@gmail.com>
2023-11-01 16:13:31 -0300

    spring-oss-project initial commit

---
+++ spring-oss-project/spring-oss-project/src/main/java/br/com/project/springossproject/SpringOssProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.springossproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOssProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOssProjectApplication.class, args);
+	}
+
+}
---
+++ spring-oss-project/spring-oss-project/src/test/java/br/com/project/springossproject/SpringOssProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.springossproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOssProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
