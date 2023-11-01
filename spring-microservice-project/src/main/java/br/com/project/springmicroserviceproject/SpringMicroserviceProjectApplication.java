05636e607e6f803c716892f8f5a899cfa6a1d831
Renan Henrique Garcia Tito <renan.tito2@gmail.com>
2023-11-01 15:25:49 -0300

    Bean configurated and imported spring Beans name explorer

--- spring-microservice-project/src/main/java/br/com/project/springmicroserviceproject/SpringMicroserviceProjectApplication.java
+++ spring-microservice-project/src/main/java/br/com/project/springmicroserviceproject/SpringMicroserviceProjectApplication.java
@@ -1,7 +1,12 @@
 package br.com.project.springmicroserviceproject;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringMicroserviceProjectApplication {
@@ -9,5 +14,17 @@ public class SpringMicroserviceProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMicroserviceProjectApplication.class, args);
 	}
-	
+
+	@Bean 
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.print("Lets inspect the beans provided by Spring Boot"); 
+			String[] beanNames = ctx.getBeanDefinitionNames(); 
+			Arrays.sort(beanNames); 
+			for(String beanName : beanNames) {
+				System.out.print("BEAN-NAME: ".concat(beanName).concat("\n"));
+			} 
+		}; 
+	}
+
 }
