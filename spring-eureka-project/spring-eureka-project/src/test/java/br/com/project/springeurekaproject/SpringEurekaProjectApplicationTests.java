889054a2da25c7b1b55e0a78c5d8da88aa0b05b5
Renan Henrique Garcia Tito <renan.tito2@gmail.com>
2023-11-06 20:40:06 -0300

    commit Eureka Server Project and your yml properties

---
+++ spring-eureka-project/spring-eureka-project/src/test/java/br/com/project/springeurekaproject/SpringEurekaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.springeurekaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
